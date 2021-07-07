e7e4bdbbda8c99a8883556e96004f4c631b5a500
Natan Williams <natan.williams123@gmail.com>
2021-07-07 23:46:40 +0200

    WIP / Working

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-import styles from './app.scss'
+import styles from "./app.scss";
 
-import Component from "./components/Component";
+import ProductList from "./components/ProductList";
 
-export default () => (<div className={styles.app}>
-    <Component />
-</div>);
+import data from "./product_list.json";
+
+export default () => (
+  <div className={styles.app}>
+    <div className={styles.main}>
+      <div>
+        <ProductList products={data} />
+      </div>
+    </div>
+  </div>
+);
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import styles from "./ProductCard.scss";
+
+export const ProductCard = ({ product }) => (
+  <div className={styles.productCard}>
+    <div className={styles.productImage}>
+      {product.filename && <img src={product.filename} />}
+    </div>
+    <div className={styles.productData}>
+      <div className={styles.brand_name}>{product.brand_name}</div>
+      <div className={styles.product_name}>{product.product_name}</div>
+      <div className={styles.actual_price}>{`${new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(product.actual_price)}`}</div>
+    </div>
+  </div>
+);
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    brand_name: PropTypes.string.isRequired,
+    product_name: PropTypes.string.isRequired,
+    filename: PropTypes.string.isRequired,
+    actual_price: PropTypes.number.isRequired,
+  }),
+};
+ProductCard.defaultProps = {
+  product: {
+    brand_name: "Brand name",
+    product_name: "Product name",
+    filename: "foo",
+    actual_price: 9.99,
+  },
+};
+
+export default ProductCard;
