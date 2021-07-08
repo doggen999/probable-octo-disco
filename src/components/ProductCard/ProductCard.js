d9eb6bc417e2f17689ae48904143b436ed774244
Natan Williams <natan.williams123@gmail.com>
2021-07-08 10:16:26 +0200

    Squashed commit of the following:
    commit 4d5553a66f9906ea8abd70f8df74922e47d5dae7
    Author: Natan Williams <natan.williams123@gmail.com>
    Date:   Thu Jul 8 10:15:44 2021 +0200
    
        Update / Add default options value to currency formatter
    
    commit a87274521f641bbf0bf0703c993129e5ab5fc0d5
    Author: Natan Williams <natan.williams123@gmail.com>
    Date:   Thu Jul 8 10:14:53 2021 +0200
    
        Update / Remove dead code

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,7 +5,10 @@ import PropTypes from "prop-types";
 import styles from "./ProductCard.scss";
 
 export const ProductCard = ({ product }) => {
-  const formatCurrency = (value, options) => {
+  const formatCurrency = (
+    value,
+    options = { locale: "en-US", currency: "USD" }
+  ) => {
     return new Intl.NumberFormat(options.locale, {
       style: "currency",
       currency: options.currency,
@@ -20,18 +23,8 @@ export const ProductCard = ({ product }) => {
         <div className={styles.brand_name}>{product.brand_name}</div>
         <div className={styles.product_name}>{product.product_name}</div>
         <div className={styles.actual_price}>
-          {formatCurrency(product.actual_price, {
-            locale: "en-US",
-            currency: "USD",
-          })}
+          {formatCurrency(product.actual_price)}
         </div>
-        <div className={styles.actual_price}>{`${new Intl.NumberFormat(
-          "en-US",
-          {
-            style: "currency",
-            currency: "USD",
-          }
-        ).format(product.actual_price)}`}</div>
       </div>
     </div>
   );
